71c1ca0d92a80bc5ea18b80d8f06c0522945e9dd
Carlos Gabriel Sousa <carlos.desenvolvedor.dev.@gmail.com>
2025-09-14 10:03:53 -0300

    commit

---
+++ Atividade 07 - Bibliotecas.py
@@ -0,0 +1,17 @@
+import math
+
+math.sqrt(25)
+math.log(1024)
+math.cos(45)
+
+import math as m
+
+m.sqrt(25)
+m.log2(1024)
+m.cos(45)
+
+from math import sqrt , log2 , cos
+
+sqrt(25)
+log2(1024)
+cos(45)
